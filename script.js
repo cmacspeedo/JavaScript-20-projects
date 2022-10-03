32a0fc565d5e0706e060b8960599e20cd6acdb3b
cmacspeedo <cmcacspeedo101@gmail.com>
2022-10-03 13:59:02 -0600

    functions

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const textBox = document.getElementById('text-box');
 
 // Dark or Light Images
 function imageMode(color) {
-    image1.src = `img/undraw_proud_coder_${color}.svg`;
-    image2.src = `img/undraw_feeling_proud_${color}.svg`;
-    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+    image1.src = `/img/undraw_game_world_${color}.svg`;
+    image2.src = `/img/undraw_mobile_development_${color}.svg`;
+    image3.src = `/img/undraw_relaxation_${color}.svg`;
 }
 function toggleDarkLightMode(isDark) {
     nav.style.backgroundColor = isDark ? 'rgb((0 0 0  / 50%)' : 'rgb(255 255 255 / 50%';
